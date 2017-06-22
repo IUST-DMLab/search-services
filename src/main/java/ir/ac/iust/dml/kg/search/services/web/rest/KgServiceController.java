5f2cb6b13eb7e06c296a66c71d919ed63ed4b6ea
Majid Asgari <majid.asgari@gmail.com>
2017-06-23 03:27:19 +0430

    singleton pattern for searcher moving all /kgservice services in one place

--- src/main/java/ir/ac/iust/dml/kg/search/services/web/rest/KgServiceController.java
+++ src/main/java/ir/ac/iust/dml/kg/search/services/web/rest/KgServiceController.java
@@ -1,16 +1,30 @@
 package ir.ac.iust.dml.kg.search.services.web.rest;
 
 import io.swagger.annotations.Api;
+import ir.ac.iust.dml.kg.search.logic.Searcher;
+import ir.ac.iust.dml.kg.search.logic.data.ResultEntity;
+import ir.ac.iust.dml.kg.search.logic.data.SearchResult;
 import ir.ac.iust.dml.kg.search.logic.kgservice.KgServiceLogic;
 import ir.ac.iust.dml.kg.search.logic.kgservice.data.*;
+import ir.ac.iust.dml.kg.search.services.Types.APIAnswerList;
+import ir.ac.iust.dml.kg.search.services.Types.APIEntity;
+import ir.ac.iust.dml.kg.search.services.Types.APIPropertyGroup;
+import ir.ac.iust.dml.kg.search.services.Types.APIPropertySingle;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/kgservice")
 @Api(tags = "kgservice", description = "سرویس‌های درخواست شده از مرکز تحقیقات")
 public class KgServiceController {
 
+    final private Searcher searcher = Searcher.getInstance();
     final private KgServiceLogic kgServiceLogic = new KgServiceLogic();
+
+    public KgServiceController() throws Exception {
+    }
 
     @RequestMapping(value = "/struct/getparent", method = RequestMethod.GET)
     @ResponseBody
@@ -41,4 +55,57 @@ public class KgServiceController {
     Entities getEntitiesOfClass(@RequestParam String classUrl) {
         return kgServiceLogic.getEntitiesOfClass(classUrl);
     }
+
+
+    @RequestMapping(value = "/getentity", method = RequestMethod.GET)
+    @ResponseBody
+    public APIAnswerList getentity(@RequestParam(required = false) String query, @RequestParam(required = false) int resultCount) throws Exception {
+        SearchResult uiResults = searcher.search(query);
+        APIAnswerList list = new APIAnswerList();
+        int order = 1;
+        try {
+            for (ResultEntity uiResult : uiResults.getEntities()) {
+                if (uiResult.getResultType() == ResultEntity.ResultType.Entity) {
+                    APIEntity apiEntity = new APIEntity(order++, uiResult.getTitle(), uiResult.getLink(), uiResult.getConfidence());
+                    list.addAnswer(apiEntity);
+                }
+                if (list.getAnswer().size() >= resultCount)
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+    @RequestMapping(value = "/getprops", method = RequestMethod.GET)
+    @ResponseBody
+    public APIAnswerList getProps(@RequestParam(required = false) String query, @RequestParam(required = false) int resultCount) throws Exception {
+        SearchResult uiResults = searcher.search(query);
+        APIAnswerList list = new APIAnswerList();
+
+        try {
+            int order = 1;
+            for (ResultEntity uiResult : uiResults.getEntities()) {
+                if (uiResult.getResultType() == ResultEntity.ResultType.RelationalResult) {
+                    //Entity OR value?
+                    String resultValueType = (uiResult.getLink() != null && uiResult.getLink().toLowerCase().contains("resource")) ? "Entity" : "Value";
+
+                    //Inner list --> Each item in a separate list (temporarily)
+                    List<APIPropertySingle> innerResultList = new ArrayList<>();
+                    innerResultList.add(new APIPropertySingle(uiResult.getTitle(), resultValueType, uiResult.getReferenceUri(), uiResult.getLink()));
+
+                    APIPropertyGroup propertyGroup = new APIPropertyGroup(order++, innerResultList, uiResult.getConfidence());
+                    list.addAnswer(propertyGroup);
+                }
+                if (list.getAnswer().size() >= resultCount)
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 }
