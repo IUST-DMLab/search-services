2b85c52ae4b544940998a228cd38c74242da7330
Majid Asgari <majid.asgari@gmail.com>
2018-01-04 03:12:08 +0330

    add support of caching mechanism to kg service methods

--- src/main/java/ir/ac/iust/dml/kg/search/services/web/rest/KgServiceLogicServiceWrapper.java
+++ src/main/java/ir/ac/iust/dml/kg/search/services/web/rest/KgServiceLogicServiceWrapper.java
@@ -2,7 +2,6 @@ package ir.ac.iust.dml.kg.search.services.web.rest;
 
 import ir.ac.iust.dml.kg.search.logic.kgservice.KgServiceLogic;
 import ir.ac.iust.dml.kg.search.logic.kgservice.data.*;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -10,41 +9,40 @@ import org.springframework.stereotype.Service;
  * To avoid adding spring dependency to searcher library, we wrote a wrapper class to use cache feature of spring
  * with ease.
  */
-@CacheConfig(cacheNames = "kgservice")
 @Service
 public class KgServiceLogicServiceWrapper extends KgServiceLogic {
 
-  @Cacheable
+  @Cacheable("getParent")
   @Override
   public ParentNode getParent(String childUrl) {
     return super.getParent(childUrl);
   }
 
-  @Cacheable
+  @Cacheable("getChildren")
   @Override
   public ChildNodes getChildren(String parentUrl) {
     return super.getChildren(parentUrl);
   }
 
-  @Cacheable
+  @Cacheable("getClassInfo")
   @Override
   public ClassInfo getClassInfo(String url) {
     return super.getClassInfo(url);
   }
 
-  @Cacheable
+  @Cacheable("getEntityInfo")
   @Override
   public EntityData getEntityInfo(String url) {
     return super.getEntityInfo(url);
   }
 
-  @Cacheable
+  @Cacheable("getEntityClasses")
   @Override
   public EntityClasses getEntityClasses(String url) {
     return super.getEntityClasses(url);
   }
 
-  @Cacheable
+  @Cacheable("getEntitiesOfClass")
   @Override
   public Entities getEntitiesOfClass(String classUrl, int page, int pageSize) {
     return super.getEntitiesOfClass(classUrl, page, pageSize);
